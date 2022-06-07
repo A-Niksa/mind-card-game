99655bc5ea0f29bf4f6988dedfa85ab24caf9afc
Alibanayeean <a.banayeean@gmail.com>
2022-06-07 18:19:25 +0430

    basics of ui

---
+++ src/main/java/frontend/gui/MainFrame.java
@@ -0,0 +1,40 @@
+package frontend.gui;
+
+import config.ConfigClass;
+import frontend.gui.firstMenuPage.FirstMenuPage;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private JPanel currentPanel;
+
+    public MainFrame() {
+        FirstMenuPage firstMenuPage = new FirstMenuPage(this);
+        setCurrentPanel(firstMenuPage);
+        configureFrame();
+        repaintFrame();
+    }
+
+    public void setCurrentPanel(JPanel currentPanel) {
+        this.currentPanel = currentPanel;
+        getContentPane().removeAll();
+        getContentPane().add(this.currentPanel);
+
+        repaintFrame();
+    }
+
+    private void configureFrame() {
+        setSize(new Dimension(ConfigClass.FRAME_WIDTH, ConfigClass.FRAME_HEIGHT));
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void repaintFrame() {
+        repaint();
+        revalidate();
+    }
+}
