52c812d6519f52643e3da028e7761cf8c81ca240
Arsha <ar.niksa@outlook.com>
2022-06-16 01:50:29 +0430

    updated literals

--- src/main/java/utils/jsonparsing/literals/dataeggs/gamestate/GameStateEgg.java
+++ src/main/java/utils/jsonparsing/literals/dataeggs/gamestate/GameStateEgg.java
@@ -74,7 +74,7 @@ public class GameStateEgg extends DataEgg {
         shouldShowSmallestCards = game.getNinjaHandler().shouldShowSmallestCards();
         smallestCardsList = game.getNinjaHandler().getSmallestCardsList();
 
-        numberOfNinjaCards = game.getDeck().getNumberOfNinjaCards();
+        numberOfNinjaCards = game.getDeck().getNumberOfNinjaCards(); // dummy push
     }
 
     public int getNumberOfHumans() {
