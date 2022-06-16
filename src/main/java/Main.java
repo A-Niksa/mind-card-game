e55ca62dcd17977127b6d0c035f065db0eac089c
Alibanayeean <a.banayeean@gmail.com>
2022-06-16 13:07:51 +0430

    add music player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import MusicPlayer.MP3Player;
 import frontend.client.ClientNetwork;
 import frontend.gui.firstMenuPage.FirstMenuPage;
 import utils.config.ConfigFetcher;
@@ -5,20 +6,53 @@ import utils.config.ConfigFetcher;
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import javazoom.jl.player.Player;
+import utils.config.DefaultConfig;
+
+import java.io.*;
 
 public class Main {
     private static JFrame frame;
 
+    public static void playMusic(String path1, String path2){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    MP3Player mp3Player = new MP3Player(path1);
+                    mp3Player.play();
+                    try {
+                        Thread.sleep((5 * 60 + 21) * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    MP3Player mp3Player2 = new MP3Player(path2);
+                    mp3Player2.play();
+                    try {
+                        Thread.sleep((5 * 60 + 15) * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
 
     public static void main(String[] args) {
+        playMusic(DefaultConfig.publicNameForPath + "m.mp3", DefaultConfig.publicNameForPath + "m2.mp3");
 
+        
         var clientNetwork = new ClientNetwork();
         int playerId = clientNetwork.addNewPlayer();
-
         new FirstMenuPage(clientNetwork, playerId);
-
 
     }
 }
+
+
+
---
+++ src/main/java/MusicPlayer/MP3Player.java
@@ -0,0 +1,43 @@
+package MusicPlayer;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class MP3Player {
+    private final String mp3FileToPlay;
+    private javazoom.jl.player.Player jlPlayer;
+
+    public MP3Player(String mp3FileToPlay) {
+        this.mp3FileToPlay = mp3FileToPlay;
+    }
+
+    public void play() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(mp3FileToPlay);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            jlPlayer = new javazoom.jl.player.Player(bufferedInputStream);
+        } catch (Exception e) {
+            System.out.println("Problem playing mp3 file " + mp3FileToPlay);
+            System.out.println(e.getMessage());
+        }
+
+        new Thread() {
+            public void run() {
+                while (true){
+                    try {
+                        jlPlayer.play();
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                }
+            }
+        }.start();
+
+
+    }
+
+    public void close() {
+        if (jlPlayer != null) jlPlayer.close();
+    }
+}
